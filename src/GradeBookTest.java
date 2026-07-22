8fd761cf8d57f00dfa42be5611653252a4152a93
Areej Maryam <areejmaryam406@gmail.com>
2026-07-22 12:48:25 +0500

    Update GradeBookTest.java

--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+class GradeBook{
+	public void display(){
+		System.out.println("Welcome to GradeBook!");
+	}
+}
 
-public class GradeBookTest {
+public class GradeBookTest{
+	public static void main(String[]args){
+		GradeBook gradebook=new GradeBook();
+		gradebook.display();
+	}
 }
