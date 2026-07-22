fef49dd0c42917852943a8975c49ea1252c570c9
Areej Maryam <areejmaryam406@gmail.com>
2026-07-22 12:46:07 +0500

    Update CircleTest.java

--- src/CircleTest.java
+++ src/CircleTest.java
@@ -1,16 +1,19 @@
-class Circle {
-  double radius=6;
+class Circle{
+	double radius=3.50;
+	public double calculateArea(){
+		return Math.PI*radius*radius;
+	}
 
-    public double calculatearea() {
-        return Math.PI*radius*radius;
-    }
-    public double calculateperimeter() {
-        return Math.PI*radius*radius;
-    }
+	public double calculatePerimeter(){
+		return 2*Math.PI*radius;
+	}
 }
 
 public class CircleTest{
-    public static void main(String[] args) {
-
-    } 
-}
+	public static void main(String[]args){
+		Circle circle=new Circle();
+	System.out.printf("Radius of a circle: %.2f%n",circle.radius); 
+	System.out.printf("Area of a circle: %.2f%n",circle.calculateArea());
+	System.out.printf("Perimeter of a circle: %.2f%n",circle.calculatePerimeter());
+	}
+} 
